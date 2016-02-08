509d89a387d26e01523a1fb488a4df005e10a8ee
Isaac Getto <isaac.getto@gmail.com>
2016-02-08 17:39:34 -0500

    add HttpVersion

---
+++ src/main/java/server/HttpVersion.java
@@ -0,0 +1,41 @@
+package server;
+
+/**
+ * Created by isaac on 2/8/16.
+ */
+public class HttpVersion {
+    public static final HttpVersion HTTP_V_1_1 = new HttpVersion("HTTP/1.1");
+    private int majorVersion;
+    private int minorVersion;
+
+    public HttpVersion(String httpVersion) {
+        int majorVersionIndex = httpVersion.indexOf('/') + 1;
+        int minorVersionIndex = httpVersion.indexOf('.') + 1;
+
+        char majorVersionChar = httpVersion.charAt(majorVersionIndex);
+        char minorVersionChar = httpVersion.charAt(minorVersionIndex);
+
+        majorVersion = Character.getNumericValue(majorVersionChar);
+        minorVersion = Character.getNumericValue(minorVersionChar);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        HttpVersion that = (HttpVersion) o;
+
+        if (majorVersion != that.majorVersion) return false;
+        return minorVersion == that.minorVersion;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = majorVersion;
+        result = 31 * result + minorVersion;
+        return result;
+    }
+
+}
---
+++ src/test/java/server/HttpVersionTest.java
@@ -0,0 +1,27 @@
+package server;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by isaac on 2/8/16.
+ */
+public class HttpVersionTest {
+
+    @Test
+    public void testHttpVersion_1_1() throws Exception {
+        assertEquals(new HttpVersion("HTTP/1.1"), HttpVersion.HTTP_V_1_1);
+        assertEquals(HttpVersion.HTTP_V_1_1, new HttpVersion("HTTP/1.1"));
+
+        assertNotEquals(new HttpVersion("HTTP/3.1"), HttpVersion.HTTP_V_1_1);
+        assertNotEquals(HttpVersion.HTTP_V_1_1, new HttpVersion("HTTP/3.1"));
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+        assertEquals(new HttpVersion("HTTP/0.0"), new HttpVersion("HTTP/0.0"));
+        assertEquals(new HttpVersion("HTTP/2.0"), new HttpVersion("HTTP/2.0"));
+        assertEquals(new HttpVersion("HTTP/4.9"), new HttpVersion("HTTP/4.9"));
+    }
+}
